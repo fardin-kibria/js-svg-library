8f48f564bed62db31c56798e4ebe1565dee04053
fardin-kibria <fardinkibria86@gmail.com>
2022-02-05 12:13:58 -0500

    tjx tracker commit

---
+++ TJX-Tracker/Application/app.js
@@ -0,0 +1,93 @@
+﻿'use strict';
+var dotenv = require("dotenv").config()
+var debug = require('debug');
+var express = require('express');
+var path = require('path');
+var logger = require('morgan');
+var cookieParser = require('cookie-parser');
+var bodyParser = require('body-parser');
+var cors = require('cors')
+var customerRoutes = require("./routes/customers")
+var orderRoutes = require("./routes/orders")
+var productRoutes = require("./routes/products")
+var csrRoutes = require("./routes/csr")
+
+
+var server; 
+var app = express();
+
+// view engine setup
+app.set('views', path.join(__dirname, 'views'));
+app.set('view engine', 'ejs');
+
+app.use(cors())
+app.use(logger('dev'));
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({ extended: false }));
+app.use(cookieParser());
+
+app.use(express.static(path.join(__dirname, '/FrontEndDev')));
+
+
+
+
+
+
+app.use("/api/customers", customerRoutes)
+app.use("/api/orders", orderRoutes)
+app.use("/api/products", productRoutes)
+app.use("/csr", csrRoutes)
+
+
+// app.get("/", (req, res) => {
+//     res.render("index")
+// })
+
+// catch 404 and forward to error handler
+app.use(function (req, res, next) {
+    var err = new Error('Not Found');
+    err.status = 404;
+    next(err);
+});
+
+// error handlers
+
+// development error handler
+// will print stacktrace
+if (app.get('env') === 'development') {
+    app.use(function (err, req, res, next) {
+        res.status(err.status || 500);
+        res.render('error', {
+            message: err.message,
+            error: err
+        });
+    });
+}
+
+// production error handler
+// no stacktraces leaked to user
+app.use(function (err, req, res, next) {
+    res.status(err.status || 500);
+    res.render('error', {
+        message: err.message,
+        error: err.status
+    });
+});
+
+app.set('port', process.env.PORT || 3000);
+
+exports.listen = function () {
+    server = app.listen(app.get('port'), function () {
+        debug('Express server listening on port ' + server.address().port);
+    });
+}
+
+exports.close = function () {
+    server.close(() => {
+        debug('Server stopped.');
+    });
+}
+
+this.listen();
+
+//module.exports = app;
